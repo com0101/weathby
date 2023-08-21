4efeb463ff445f19e631643cabf882cd2320d136
sophie_wang <jjonerepulic0101@gmail.com>
2023-08-21 22:56:51 +0800

    finish view object

--- app/src/main/java/com/example/weathby/home/HomeVO.kt
+++ app/src/main/java/com/example/weathby/home/HomeVO.kt
@@ -1,3 +1,32 @@
 package com.example.weathby.home
 
-sealed class HomeVO
+sealed class HomeVo(val id: Int)
+data class CityCard(
+    val cityId: Int,
+    val cityName: String,
+    val day: String,
+    val temp: String,
+    val wind: String,
+    val wet: String,
+    val rain: String,
+    val isMarked: Boolean,
+    val dayTem: List<CityDayTemp>
+) : HomeVo(cityId)
+data class CityDayTemp(
+    val day: String,
+    val icon: IconType,
+    val maxTemp: String,
+    val minTemp: String
+)
+data class CityHourTemp(
+    val cityId: Int,
+    val time: String,
+    val temp: String,
+    val iconType: IconType
+) : HomeVo(cityId)
+
+data class EmptyState(val cityId: Int) : HomeVo(cityId)
+data class ErrorState(val cityId: Int, val message: String) : HomeVo(cityId)
+enum class IconType {
+    RAIN, SUN, CLOUD
+}
--- app/src/main/java/com/example/weathby/search/CitiesVO.kt
+++ app/src/main/java/com/example/weathby/search/CitiesVO.kt
@@ -1,3 +1,7 @@
 package com.example.weathby.search
 
+import com.example.weathby.home.HomeVo
+
 sealed class CitiesVO
+object EmptyState : CitiesVO()
+data class ErrorState(val message: String) : CitiesVO()
